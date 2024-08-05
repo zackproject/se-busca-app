e97d25500d3997ede1e0c90c2566092c4aec94f0
zksama <44408822+zackproject@users.noreply.github.com>
2024-08-05 23:56:17 +0200

    Logic game (#2)
    * Click character change panel
    
    * Hide & timeout on correct Character
    
    * Update packages

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,12 +1,59 @@
 import { StyleSheet, View, Pressable, Image, Button } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { MaterialIcons } from "@expo/vector-icons";
-import sebusca from "../utils/images.js";
 // import Animated from "react-native-reanimated";
 import { NavbarGame } from "./NavbarGame.jsx";
+import { randPercent } from "../utils/randNum.js";
+import { useEffect, useState, useRef } from "react";
+import { Wanted } from "../utils/Wanted.js";
+
 export function Game() {
-  const image = sebusca.clasic[0].image;
   const insets = useSafeAreaInsets();
+  const [characterList, setCharacterList] = useState();
+  // const intervalRef = useRef(null); // useRef to store interval ID
+  const [myCharacter, setMyCharacter] = useState();
+  const [time, setTime] = useState(60);
+  const [score, setScore] = useState(0);
+  const [numberOfCharacters, setNumberOfCharacters] = useState(4);
+
+  const [isPlaying, setPlaying] = useState(true);
+  const [characterXY, setCharacterXY] = useState([
+    randPercent() + "%",
+    randPercent() + "%",
+  ]);
+
+  const generatePanel = () => {
+    const myChar = Wanted.getRandCharacter();
+    setCharacterList(Wanted.getRandPanel(myChar, numberOfCharacters));
+    setMyCharacter(myChar);
+  };
+
+  const callCorrect = () => {
+    if (isPlaying) {
+      setScore(score + 1);
+      setPlaying(false);
+      setNumberOfCharacters((prev) => {
+        return Wanted.addCharactersPanel(score, prev);;
+      });
+
+      setTimeout(() => {
+        setCharacterXY([randPercent() + "%", randPercent() + "%"]);
+        setPlaying(true);
+        generatePanel();
+      }, 1000);
+    }
+  };
+
+  const callInCorrect = () => {
+    alert("Mal");
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      generatePanel();
+    }
+  }, []);
+
   return (
     <View
       style={{
@@ -17,25 +64,54 @@ export function Game() {
       }}
     >
       <View style={styles.element1}>
-        <NavbarGame image={image} time={40} score={132} />
+        {myCharacter && (
+          <NavbarGame
+            image={Wanted.getCharacterImage(myCharacter)}
+            name={Wanted.getCharacterName(myCharacter)}
+            time={time}
+            score={score}
+          />
+        )}
       </View>
       <View style={styles.element2}>
-        {Array.from({ length: 10 }).map((_, index) => (
-          <Image
-            key={index}
-            source={image}
+        {characterList &&
+          isPlaying &&
+          characterList.map((e, i) => (
+            <Pressable
+              key={i}
+              style={{
+                position: "absolute",
+                bottom: randPercent() + "%",
+                left: randPercent() + "%",
+              }}
+              onPress={callInCorrect}
+            >
+              <Image
+                source={Wanted.getCharacterImage(e)}
+                alt={Wanted.getCharacterName(e)}
+                style={[styles.imageCharacter]}
+              />
+            </Pressable>
+          ))}
+        {myCharacter && (
+          <Pressable
             style={{
-              height: 70,
-              width: 70,
               position: "absolute",
-              bottom: index + "0%",
-              left: index + "0%",
+              bottom: characterXY[0],
+              left: characterXY[1],
             }}
-          ></Image>
-        ))}
+            onPress={callCorrect}
+          >
+            <Image
+              source={Wanted.getCharacterImage(myCharacter)}
+              alt={Wanted.getCharacterName(myCharacter)}
+              style={[styles.imageCharacter, { zIndex: 1 }]}
+            />
+          </Pressable>
+        )}
       </View>
       <View style={styles.element3}>
-        <Pressable style={styles.btn} onPress={() => alert("Pause")}>
+        <Pressable style={styles.btn} onPress={callCorrect}>
           <MaterialIcons name="play-circle-outline" color="white" size={30} />
         </Pressable>
         <MaterialIcons name="music-note" color="white" size={30} />
@@ -72,5 +148,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     paddingVertical: 10,
+  },
+
+  imageCharacter: {
+    height: 70,
+    width: 70,
   },
 });
---
+++ utils/Wanted.js
@@ -0,0 +1,48 @@
+import sebusca from "./images";
+import { randNum } from "./randNum";
+
+const characterList = sebusca.clasic;
+// const specialCharacter = sebusca.special;
+
+// const special = sebusca.special;
+// const myCharacter = null;
+export class Wanted {
+  static getRandPanel(myCharacter, numberOfCharacters) {
+    let i = 0;
+    let newLlista = [];
+    // Quant personatges repetits volem
+    while (i < numberOfCharacters - 1) {
+      let rand = randNum(0, characterList.length - 1);
+      let randCharacter = characterList[rand];
+      if (randCharacter !== myCharacter) {
+        newLlista.push(randCharacter);
+        i++;
+      }
+    }
+    return newLlista;
+  }
+
+  static addCharactersPanel(score, numberOfCharacters) {
+    if (score % 50 == 0 && score > 5) {
+      return parseInt(numberOfCharacters / 2);
+    }
+    if (score % 5 == 0 && score > 5) {
+      return numberOfCharacters + 4;
+    }
+    return numberOfCharacters;
+  }
+
+  static getRandCharacter() {
+    let rand = randNum(0, characterList.length - 1);
+    myCharacter = characterList[rand];
+    return myCharacter;
+  }
+
+  static getCharacterImage(myCharacter) {
+    return myCharacter.image;
+  }
+
+  static getCharacterName(myCharacter) {
+    return myCharacter.name;
+  }
+}
